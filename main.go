8b62f3a8a5d10269020bb62af555614ab3a4c382
名字已消失了 <48387476+mzyxsl@users.noreply.github.com>
2025-11-12 00:56:00 +0800

    Update owner accounts in main.go
    Replaced hardcoded owner accounts with specific values.

--- main.go
+++ main.go
@@ -266,8 +266,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 3305114625)
+	sus = append(sus, 1207372433)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
